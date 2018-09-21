f4c0aaaad5739c73c9eb3305568b00219e8b004c
MatthewMitiaguin <42827954+MatthewMitiaguin@users.noreply.github.com>
2018-09-21 18:18:48 +1000

    Updating header php
    Added link to display sales

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,7 @@
 <nav>
   <p>
     <a href="show_stock.php">Show Stock</a> |
+    <a href="show_sales.php">Show sales</a> |
     <a href="add_new_stock.php">Add New Stock</a>
     | <a href="./AddSales.php">Add Sale Record</a>
     | <a href="./DeleteSales.php">Delete Sale Record</a>
