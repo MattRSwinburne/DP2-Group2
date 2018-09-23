dc138c9e1b72c271ef51dea08bb5608f9c4b8582
MatthewMitiaguin <42827954+MatthewMitiaguin@users.noreply.github.com>
2018-09-23 21:41:48 +1000

    Forgot to upload previously

---
+++ inc/sale_search.php
@@ -0,0 +1,14 @@
+<label for="item_id">Item id:</label>
+<input type="number" name="item_id" id="item_id"/>
+<br />
+<label for="qty">Quantity:</label>
+<input type="number" name="qty" id="qty" />
+<br />
+<label for="total">Total:</label>
+<input type="number" name="total" id="total" />
+<br />
+<label for="date_time">Date: </label>
+<input type="date_time" name="date_time" id="date_time" />
+<br />
+<button type="submit">Submit!</button>
+<button type="reset">Reset</button>
