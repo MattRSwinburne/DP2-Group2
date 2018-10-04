66486b063ed8e479e6855dffd8bc2a64e37a4823
T <t.russell.au@gmail.com>
2018-10-04 15:20:14 +1000

    Added search to sales page.  (+ small sales page improvements.)

--- show_sales.php
+++ show_sales.php
@@ -17,6 +17,15 @@
        ?>
     </aside>
 
+    <aside name="sales_search" id="sales_search">
+      <form action="show_sales.php">
+        <input type="hidden" name="search" id="search" value="sales" />
+        <?php include "inc/sales_search.php" ?>
+        <button type="submit">Submit!</button>
+        <a href="show_sales.php"><button type="button">Reset</button></a>
+      </form>
+    </aside>
+
     <section>
 
       <?php
@@ -30,13 +39,38 @@
           echo "<p>DB Connection Error: " . mysqli_connect_error() . "</p>";
         }
 
-        $query = "SELECT * FROM sales";
+        $query = "SELECT sales.id, stock.item_name, stock.brand, sales.qty, sales.total, sales.date_time
+                  FROM sales
+                  INNER JOIN stock ON stock.id=sales.item_id
+                  WHERE sales.id != '0'";
+
+
+        if (isset($_GET["search"])) {
+          if ($_GET["item_name"] != "") {
+            $query .= "AND stock.item_name LIKE '%" . $_GET["item_name"] . "%'";
+          }
+          if ($_GET["brand"] != "") {
+            $query .= "AND stock.brand LIKE '%" . $_GET["brand"] . "%'";
+          }
+          if ($_GET["qty"] != "") {
+            $query .= "AND sales.qty" . $_GET["qty_range_symbol"] . "'" . $_GET["qty"] . "'";
+          }
+          if ($_GET["total"] != "") {
+            $query .= "AND sales.total" . $_GET["total_range_symbol"]  . "'" . $_GET["total"]*100 . "'";
+          }
+          if ($_GET["date"] != "") {
+            $query .= "AND DATE(sales.date_time)='" . $_GET["date"] . "'";
+          }
+        }
+        //if your query is buggy  (check you're putting your data in the right box before anything, SELF :/)
+        // echo $query;
         $result = mysqli_query($db_connect, $query);
         if (mysqli_num_rows($result) > 0) {
           ?>
           <table>
             <tr>
               <th>Item</th>
+              <th>Brand</th>
               <th>Quantity</th>
               <th>Total</th>
               <th>Date</th>
@@ -45,9 +79,10 @@
             <?php
             foreach ($result as $row) {
               echo "<tr>\n";
-              echo "<td>" . $row['item_id'] . "</td>\n";
+              echo "<td>" . $row['item_name'] . "</td>\n";
+              echo "<td>" . $row['brand'] . "</td>\n";
               echo "<td>" . $row['qty'] . "</td>\n";
-              echo "<td>" . $row['total'] . "</td>\n";
+              echo "<td>" . "$" . number_format($row['total']/100, 2) . "</td>\n";
               echo "<td>" . $row['date_time'] . "</td>\n";
               echo "<td><a href='edit_sales.php?saleid=". $row['id']."'>edit</a>";
               echo "</tr>\n";
